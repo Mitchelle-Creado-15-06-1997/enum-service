608cfeb806d3ca64f646c2964b150f4d36acdba7
Mitchelle Creado <Mitchelle.Creado@neo-world.com>
2022-06-09 18:08:40 +0530

    added prisma

--- biz/enum.biz.js
+++ biz/enum.biz.js
@@ -1,10 +1,11 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+// const { PrismaClient } = require('@prisma/client');
+// const prisma = new PrismaClient();
 const QUERY = require('../constants/queryRepo');
 const QueryBuilderBiz = require('./helpers/query-builder.biz');
-
+const SqlBiz = require('./helpers/sql.biz')
 class EnumBiz {
 	constructor() {
+		this.sqlBiz = new SqlBiz();
 		this.queryBuilderBiz = new QueryBuilderBiz();
 	}
 
@@ -39,8 +40,8 @@ class EnumBiz {
 				if(data.mapping_name) query_data = (query_data ? query_data + ' and ' : '') + `evm.mapping_name = '${data.mapping_name}'`; 
 				if(data.type_key) query_data = (query_data ? query_data + ' and ' : '') + `eoe.type_key = '${data.type_key}'`; 
 				query_data = (query_data ? query_data + ' and ' : '') + `evm.is_active = 1 and ev.is_active = 1`;
-
-				result = await prisma.$queryRawUnsafe(QUERY.SQL.SELECT['enum'] + query_data)
+				result = await this.sqlBiz.get_one(null, QUERY.SQL.SELECT['enum'] + query_data);
+				// result = await prisma.$queryRawUnsafe(QUERY.SQL.SELECT['enum'] + query_data)
 				resolve(result);
 			} catch(error){
 				return reject(error);
